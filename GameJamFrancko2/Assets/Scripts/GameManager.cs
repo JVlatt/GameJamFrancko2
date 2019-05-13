d084287f7646620a21c9efdb32a2d95a2b3ad757
Unknown <matthieu.manuellan@gmail.com>
2019-05-13 10:30:07 +0200

    Setup Unity

---
+++ GameJamFrancko2/Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Script
+{
+    public class GameManager
+    {
+        static GameManager _manger = null;
+
+        public static GameManager GetManager()
+        {
+            if (_manger == null)
+            {
+                _manger = new GameManager(); // patern singleton
+            }
+            return _manger;
+        }
+
+    }
+}
